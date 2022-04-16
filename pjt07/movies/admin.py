d86e6a953442cf377c712eb01903f634eaf2d314
sskong777 <sskong777@gmail.com>
2022-04-16 12:55:26 +0900

    0415 pjt

--- pjt07/movies/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Movie,Comment
-from django.contrib.auth.admin import UserAdmin
-# from django.contrib.auth.admin impor
-# Register your models here.
-
-admin.site.register(Movie)
-admin.site.register(Comment)
